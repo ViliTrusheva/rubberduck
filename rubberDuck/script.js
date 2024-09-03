bdf63d57f1bca370d17efa8d7d405103aa9e9e1d
Vili <veli0119@stud.kea.dk>
2024-09-03 15:44:55 +0200

    button

--- rubberDuck/script.js
+++ rubberDuck/script.js
@@ -1,1 +1,11 @@
 console.log("hello");
+
+const submitBtn = document.getElementById("submitBtn");
+
+submitBtn.addEventListener("click", function () {
+  const problemInput = document.getElementById("problemInput").value;
+
+  //   console.log(problemInput);
+
+  alert(problemInput);
+});
